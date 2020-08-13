cf1d0026ce60b1b0ebacb16c8e113b1bcebc9666
MAX-NETAILAB-DEV <netailab.dev@gmail.com>
2020-08-13 18:31:04 -0500

    Agregamos los metodos SetOnClickListener en los botones de la Clase MainActivity

--- app/src/main/java/com/netailab/maxexpress/MainActivity.java
+++ app/src/main/java/com/netailab/maxexpress/MainActivity.java
@@ -3,12 +3,35 @@ package com.netailab.maxexpress;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnConductor,btnCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnConductor = findViewById(R.id.soy_conductor);
+        btnConductor = findViewById(R.id.soy_conductor);
+
+        /*Boton para seleccionar Chofer*/
+        btnConductor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        /*Boton para seleccionar Cliente*/
+        btnCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
