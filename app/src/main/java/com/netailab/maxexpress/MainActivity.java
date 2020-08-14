2c4b749c0d18e0b3df7ef57537a97f6177ba6dbe
MAX-NETAILAB-DEV <netailab.dev@gmail.com>
2020-08-13 19:08:54 -0500

    Implementacion de un ToolBar

--- app/src/main/java/com/netailab/maxexpress/MainActivity.java
+++ app/src/main/java/com/netailab/maxexpress/MainActivity.java
@@ -2,6 +2,7 @@ package com.netailab.maxexpress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnConductor = findViewById(R.id.soy_conductor);
-        btnConductor = findViewById(R.id.soy_conductor);
+        btnCliente = findViewById(R.id.soy_cliente);
 
         /*Boton para seleccionar Chofer*/
         btnConductor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                navegarVistaAutenticacion();
             }
         });
 
@@ -30,8 +31,14 @@ public class MainActivity extends AppCompatActivity {
         btnCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                navegarVistaAutenticacion();
             }
         });
+    }
+
+    /*Navegar a la vista de Autenticacion de usuario*/
+    private void navegarVistaAutenticacion() {
+        Intent intent = new Intent(MainActivity.this,SelectOptionAuthActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/netailab/maxexpress/SelectOptionAuthActivity.java
+++ app/src/main/java/com/netailab/maxexpress/SelectOptionAuthActivity.java
@@ -1,14 +1,23 @@
 package com.netailab.maxexpress;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Seleccionar una opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
