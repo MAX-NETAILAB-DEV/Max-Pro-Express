d02b001efb610f5a7097b3a84fe67da8e03eb9e6
MAX-NETAILAB-DEV <netailab.dev@gmail.com>
2020-08-13 19:52:10 -0500

    Implementacion de la funcionalidad de login con usuario existente en la Base de datos

--- app/src/main/java/com/netailab/maxexpress/LoginActivity.java
+++ app/src/main/java/com/netailab/maxexpress/LoginActivity.java
@@ -1,14 +1,75 @@
 package com.netailab.maxexpress;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText input_correo, input_clave;
+    Button btn_login;
+    private FirebaseAuth mAuth;
+    DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        input_correo = findViewById(R.id.input_correo);
+        input_clave = findViewById(R.id.input_clave);
+        btn_login = findViewById(R.id.btn_ingresar);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+
+    }
+
+    private void login() {
+        String email = input_correo.getText().toString();
+        String clave = input_clave.getText().toString();
+
+        if (!email.isEmpty() && !clave.isEmpty()) {
+            if (clave.length() >= 6) {
+                mAuth.createUserWithEmailAndPassword(email, clave).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login Exitoso", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "La contraseña o el password son incorrectos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 }
--- app/src/main/java/com/netailab/maxexpress/SelectOptionAuthActivity.java
+++ app/src/main/java/com/netailab/maxexpress/SelectOptionAuthActivity.java
@@ -3,11 +3,15 @@ package com.netailab.maxexpress;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    Button btnIniciarSesion,btnRegistrarme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,29 @@ public class SelectOptionAuthActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Seleccionar una opcion");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnIniciarSesion = findViewById(R.id.btn_iniciar_sesion);
+        btnRegistrarme = findViewById(R.id.btn_registrarme);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navegarVistaLogin();
+            }
+        });
+
+        btnRegistrarme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void navegarVistaLogin() {
+        Intent intent = new Intent(SelectOptionAuthActivity.this,LoginActivity.class);
+        startActivity(intent);
 
     }
 }
